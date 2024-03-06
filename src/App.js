35561b7d11a551609fbdb710f134d437e8b7bb7c
Bryttany <BryttanyBCH@protonmail.com>
2024-03-06 18:50:31 -0500

    Added a skeleton html/css for the app layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import "./styles.css"
+
 function App() {
-  return null
+  return (
+      <div className="crop-calculator">
+          <div className="crop-selection"></div>
+          <div className="right-column">
+              <div className="diagram-result"></div>
+              <div className="calculation-result"></div>
+              <div className="crops-selected"></div>
+          </div>
+      </div>
+  )
 }
 
 export default App;
